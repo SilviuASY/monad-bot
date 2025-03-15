0a10c18e0a691642eb73bce584b0e73bd290dec3
SilviuASY <160680659+SilviuASY@users.noreply.github.com>
2025-03-15 12:18:35 +0100

    displayHeader.js

--- src/displayHeader.js
+++ src/displayHeader.js
@@ -4,8 +4,8 @@ function displayHeader() {
   process.stdout.write("\x1Bc");
   console.log("========================================".magenta);
   console.log("=          Monad Testnet Bot           =".magenta);
-  console.log("=     Created by SilviuASY             =".magenta);
-  console.log("=    https://t.me/silviu_asy           =".magenta);
+  console.log("=        Created by SilviuASY          =".magenta);
+  console.log("=       https://t.me/silviu_asy        =".magenta);
   console.log("========================================".magenta);
   console.log();
 }
